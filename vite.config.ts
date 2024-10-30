706fd387cf9d4613912e18539eb73424361cf7e9
willerdev <willeratmit12@gmail.com>
2024-10-30 08:01:59 -0700

    shop linking sss

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      strategies: 'injectManifest',
-      srcDir: 'src/service-workers',
-      filename: 'cart-worker.ts',
-      injectRegister: 'auto',
-      manifest: {
-        name: 'Shopping Cart',
-        short_name: 'Cart',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
 });
