4a3879359630b4983d63df5497a20942a5d79b55
willerdev <willeratmit12@gmail.com>
2024-12-29 13:52:35 -0800

    upg

--- src/components/GoogleTranslate.tsx
+++ src/components/GoogleTranslate.tsx
@@ -1,6 +1,5 @@
 const GoogleTranslate = () => {
-    return <div id="google_translate_element"></div>;
-  };
-  
-  export default GoogleTranslate;
-  
+  return <div id="google_translate_element" style={{ marginTop: '20px' }}></div>;
+};
+
+export default GoogleTranslate;
